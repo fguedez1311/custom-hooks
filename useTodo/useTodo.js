f1b898e13657b69eb7f9d05d31cba2bb93bdfeca
fguedez <fguedez1311@gmail.com>
2022-09-06 15:40:40 -0400

    useTodos agregado

--- useTodo/useTodo.js
+++ useTodo/useTodo.js
@@ -1,6 +1,6 @@
 import { useReducer } from "react"
 import { useEffect } from "react"
-import { todoReducer } from "../08-useReducer/todoReducer"
+import { todoReducer } from "./todoReducer"
 
 
 export const useTodo = () => {
